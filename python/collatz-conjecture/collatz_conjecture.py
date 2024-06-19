1127a02f01f89f0d89ab63f691540c34fdf66786
Rob Olszewski <robsju@yahoo.com>
2024-06-18 20:23:02 -0400

    Corrected syntax in line 18 substituted == for =

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -15,7 +15,7 @@ def steps(number):
 
     while number != 1:
         i += 1
-        if number % 2 = 0: number = number / 2
+        if number % 2 == 0: number = number / 2
     
         else: number = (number * 3) + 1
 
