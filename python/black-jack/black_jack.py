7df30c9662b2bf1f75abed1ef02b362b17c0d8fc
Rob Olszewski <robsju@yahoo.com>
2024-07-08 06:30:56 -0400

    First two exercises complete

--- python/black-jack/black_jack.py
+++ python/black-jack/black_jack.py
@@ -1,11 +1,7 @@
-"""Functions to help play and score a game of blackjack.
-
-How to play blackjack:    https://bicyclecards.com/how-to-play/blackjack/
-"Standard" playing cards: https://en.wikipedia.org/wiki/Standard_52-card_deck
-"""
-
+"""Functions to help play and score a game of blackjack."""
 
 def value_of_card(card):
+    
     """Determine the scoring value of a card.
 
     :param card: str - given card.
@@ -15,10 +11,13 @@ def value_of_card(card):
     2.  'A' (ace card) = 1
     3.  '2' - '10' = numerical value.
     """
-    if "J" or "Q" or "K":
-        value = 10
-
-    pass
+    
+    COURT_CARDS = {"J","K","Q"}
+    if card == "A":
+        return 1
+    if card in COURT_CARDS:
+        return 10
+    return int(card)
 
 
 def higher_card(card_one, card_two):
@@ -31,9 +30,14 @@ def higher_card(card_one, card_two):
     2.  'A' (ace card) = 1
     3.  '2' - '10' = numerical value.
     """
+    value_one = value_of_card(card_one)
+    value_two = value_of_card(card_two)
 
-    pass
-
+    if value_one == value_two:
+        return (card_one,card_two)
+    if value_one > value_two:
+        return card_one
+    return card_two
 
 def value_of_ace(card_one, card_two):
     """Calculate the most advantageous value for the ace card.
@@ -45,10 +49,10 @@ def value_of_ace(card_one, card_two):
     2.  'A' (ace card) = 11 (if already in hand)
     3.  '2' - '10' = numerical value.
     """
-
-    pass
-
-
+    if value_of_card(card_one) + value_of_card(card_two) <= 10:
+        return 11
+    return 1
+    
 def is_blackjack(card_one, card_two):
     """Determine if the hand is a 'natural' or 'blackjack'.
 
